e144aa47bd8edb72e1b3470b58b15f10ecf2a867
Paolo <str237@gmail.com>
2020-02-01 00:22:23 +0100

    Map Generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public static int[,] Generate(int width, int height, int nElements)
+    {
+        int[,] map = new int[width, height];
+        for (int x = 0; x < height; x++) {
+            bool isElementSet = false;
+            for (int y = 0; y < width; y++) {
+                if (isElementSet) {
+                    map[x, y] = 0;
+                }
+                else {
+                    if (Random.Range(0,1) == 0) {
+                        map[x, y] = Random.Range(1, nElements);
+                        isElementSet = true;
+                    }
+                }
+            }
+        }
+        return map;
+    }
+}
