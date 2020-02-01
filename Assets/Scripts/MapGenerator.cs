46988a6e3890333901d3ba0099836b68615ef987
Paolo <str237@gmail.com>
2020-02-01 12:23:13 +0100

    Successful map rendering

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 
 public class MapGenerator : MonoBehaviour
 {
+    public Tilemap tilemap;
+    public Tile baseTile;
+    void Start() { 
+        int [,] map = Generate(8, 200, 1);
+        RenderMap(map);
+    }
     public static int[,] Generate(int width, int height, int nElements)
     {
         int[,] map = new int[width, height];
-        for (int x = 0; x < height; x++) {
+        for (int y = 0; y < height; y++) {
             bool isElementSet = false;
-            for (int y = 0; y < width; y++) {
+            for (int x = 0; x < width; x++) {
                 if (isElementSet) {
                     map[x, y] = 0;
                 }
@@ -22,5 +30,23 @@ public class MapGenerator : MonoBehaviour
             }
         }
         return map;
+    }
+    public void RenderMap(int[,] map)
+    {
+        //Clear the map (ensures we dont overlap)
+        tilemap.ClearAllTiles(); 
+        //Loop through the width of the map
+        for (int x = 0; x < map.GetUpperBound(0) ; x++) 
+        {
+            //Loop through the height of the map
+            for (int y = 0; y < map.GetUpperBound(1); y++) 
+            {
+                // 1 = tile, 0 = no tile
+                if (map[x, y] == 1) 
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), baseTile); 
+                }
+            }
+        }
     }
 }
