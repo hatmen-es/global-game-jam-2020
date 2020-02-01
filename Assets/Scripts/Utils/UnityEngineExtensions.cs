137228dc9b30d77fc53cb86aae384cfc31ec79b8
Saul Moreno Abril <saul@appfluence.com>
2020-02-01 14:05:56 +0100

    Save files

--- Assets/Scripts/Utils/UnityEngineExtensions.cs
+++ Assets/Scripts/Utils/UnityEngineExtensions.cs
@@ -1,17 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewMonoBehaviour : MonoBehaviour
+static public class UnityEngineExtensions
 {
-    // Use this for initialization
-    void Start()
+    /// <summary>
+    /// Returns the component of Type type. If one doesn't already exist on the GameObject it will be added.
+    /// </summary>
+    /// <typeparam name="T">The type of Component to return.</typeparam>
+    /// <param name="gameObject">The GameObject this Component is attached to.</param>
+    /// <returns>Component</returns>
+    static public T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        return gameObject.GetComponent<T>() ?? gameObject.AddComponent<T>();
     }
 }
