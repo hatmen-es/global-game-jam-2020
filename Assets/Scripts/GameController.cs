953042f9891df77525f9d1be8e73489035f65de8
Saul Moreno Abril <saul@appfluence.com>
2020-02-02 14:21:30 +0100

    Add properly background to tools

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -20,6 +20,7 @@ public class GameController : MonoBehaviour
     public GameObject imageTool2;
     public GameObject imageTool3;
     public GameObject imageTool4;
+    public float toolHighlightScale = 1.25f;
     private int selectedTool = 1;
     private int speed = 1;
 
@@ -70,13 +71,13 @@ public class GameController : MonoBehaviour
 
     private void resetToolScale(GameObject tool)
     {
-        tool.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 0);
+        tool.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
     }
 
     private void selectTool(GameObject tool, int selected)
     {
         resetAllToolsScale();
-        tool.GetComponent<RectTransform>().localScale += new Vector3(.5f, 0.5f, 0);
+        tool.GetComponent<RectTransform>().localScale = new Vector2(toolHighlightScale, toolHighlightScale);
         selectedTool = selected;
     }
 
@@ -122,7 +123,7 @@ public class GameController : MonoBehaviour
 
     public void GameOver()
     {
-         audioSourceSong.Stop();
+        audioSourceSong.Stop();
         audioSourceGameover.Play();
         uiGameOver.SetActive(true);
         gameOver = true;
